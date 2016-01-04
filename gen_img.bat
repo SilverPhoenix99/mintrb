f4c8d019fc32b9a0bee8b609c694f7e62bc12497
Silver Phoenix <silver.phoenix99@gmail.com>
2016-01-04 23:01:28 +0000

    Some reorganization of lexer expressions. Fixed bug in expr_cmdarg, where the string delimiter /^%[QqWwIixrs]=$/ was being excluded. Added some TODO's in string.rbrl.

--- gen_img.bat
+++ gen_img.bat
@@ -3,6 +3,6 @@
 set MINT_GEN_DIR=%~dp0
 
 @rem ember to not use ragel without machine option
-ragel -M BOF -RVp "%MINT_GEN_DIR%lexer\lexer.rbrl" | dot -Tpng "-o%MINT_GEN_DIR%gen\lexer.png"
+ragel -M test -RVp "%MINT_GEN_DIR%lexer\lexer.rbrl" | dot -Tpng "-o%MINT_GEN_DIR%gen\lexer.png"
 
 set MINT_GEN_DIR=
